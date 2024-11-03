647bc80aabd15a4d517cd19340b0f0faa3ce2885
Cameron Seaman <cameron@globaleas.org>
2024-11-03 08:59:02 -0800

    fix: prevent escape from input validation

--- game.py
+++ game.py
@@ -13,16 +13,17 @@ def game():
     while option != '1' and option != '2' and option != '3':
         print('Invalid option. Please try again.')
         option = gamefunctions.print_user_menu(username, monster)
-    while option == '1' or option == '2' or option == '3':
+    while option != '3':
         if option == '1':
             monster = gamefunctions.fight_monster(monster)
             option = gamefunctions.print_user_menu(username, monster)
         elif option == '2':
             monster = gamefunctions.sleep(monster)
             option = gamefunctions.print_user_menu(username, monster)
-        elif option == '3':
-            print('Thanks for playing!')
-            return
+        else:
+            print('Invalid option. Please try again.')
+            option = gamefunctions.print_user_menu(username, monster)
+    print('Goodbye! Thanks for playing!')
 
 if __name__ == '__main__':
     game()
